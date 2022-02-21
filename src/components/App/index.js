c2951f4cf757c7f94ab122d237db80e9d6fa6c85
Ruben Gonzalez <rubengon.uk@gmail.com>
2022-02-21 11:08:27 +0000

    Routes completed

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,18 +1,14 @@
-import Show from "../Show";
+
 import Header from '../Header/index'
 import Main from '../Main/index'
-
-
-import logo from "./logo.svg";
 import "./App.css";
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Show />
-      </header>
+      <Header />
+      <Main />
     </div>
   );
 }
