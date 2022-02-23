78c31e5d11512b6fededafe43c9ff20dcfc2b416
casey <cassandra.d.mclellan@gmail.com>
2022-02-23 14:53:11 +0000

    FEAT: added date picker input for date filter

--- src/components/FilterComponent/index.js
+++ src/components/FilterComponent/index.js
@@ -1,10 +1,19 @@
 import React from "react";
 
 import DropdownFilter from "./DropdownFilter";
+import DateSelector from "./DateSelector";
 
 function FilterComponent() {
   return (
-    <div>
+    <div
+      style={{
+        display: "grid",
+        gridTemplateColumns: "repeat(3, 1fr)",
+        gridGap: "1rem",
+      }}
+    >
+      <input placeholder="location" />
+      <DateSelector />
       <DropdownFilter />
     </div>
   );
