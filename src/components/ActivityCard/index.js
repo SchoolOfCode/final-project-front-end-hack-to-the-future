2fc3d9f01146eb6f235f88bab20ce3f188e4f0ac
Corneliu Laur <cl179971@gmail.com>
2022-02-23 09:42:16 +0000

    swipePut branch created

--- src/components/ActivityCard/index.js
+++ src/components/ActivityCard/index.js
@@ -1,16 +1,16 @@
-import React from "react";
-import TinderCard from "react-tinder-card";
+import React from 'react';
+import TinderCard from 'react-tinder-card';
 
 function ActivityCard({ activity }) {
-  return (
-    <div className="card">
-      <img src=""></img>
-      <h3>{activity.description}</h3>
-      <p>Max attendees {activity.max_attendees}</p>
-      <p>{activity.location_name}</p>
-      <p>{activity.date_time}</p>
-    </div>
-  );
+    return (
+        <div className='card'>
+            <img src=''></img>
+            <h3>{activity.description}</h3>
+            <p>Max attendees {activity.max_attendees}</p>
+            <p>{activity.location_name}</p>
+            <p>{activity.date_time}</p>
+        </div>
+    );
 }
 
 export default ActivityCard;
