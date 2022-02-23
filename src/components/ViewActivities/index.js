cd5122ec7aae35b7d11456732c12e3fe9dac79a0
Corneliu Laur <cl179971@gmail.com>
2022-02-23 10:33:28 +0000

    added put request

--- src/components/ViewActivities/index.js
+++ src/components/ViewActivities/index.js
@@ -8,7 +8,9 @@ import axios from 'axios';
 export default function ViewActivities() {
     const [activities, setActivity] = useState([]);
     const [favActivities, setFavActivities] = useState([]);
+    const [activityId, setActivityId] = useState([]);
 
+    // swipe cards direction
     useEffect(() => {
         axios
             .get('https://activity-app-backend.herokuapp.com/activities')
@@ -22,6 +24,33 @@ export default function ViewActivities() {
             // console.log(activities);
         }
     };
+
+    // update favActivities
+    useEffect(() => {
+        // PUT request using fetch with async/await
+        const updateFavActivities = async () => {
+            const requestActivity = {
+                method: 'PUT',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({
+                    title: 'React Hooks PUT Request Example',
+                }),
+            };
+            const response = await fetch(
+                // link to be changed
+                'https://activity-app-backend.herokuapp.com/activities/',
+                requestActivity
+            );
+            const data = await response.json();
+            setActivityId(data.id);
+
+            if(response === null || response !== false) {
+
+            }
+        };
+
+        updateFavActivities();
+    }, []);
 
     return (
         <div>
