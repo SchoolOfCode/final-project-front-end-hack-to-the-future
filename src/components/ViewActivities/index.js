63c62875cdf4e0794c5fdbd6679c0dc91bce8689
Corneliu Laur <cl179971@gmail.com>
2022-02-23 12:33:26 +0000

    put request update db

--- src/components/ViewActivities/index.js
+++ src/components/ViewActivities/index.js
@@ -8,8 +8,8 @@ import axios from 'axios';
 export default function ViewActivities() {
     const [activities, setActivity] = useState([]);
     const [favActivities, setFavActivities] = useState([]);
-    // const [activityId, setActivityId] = useState([]);
-
+    const [activityId, setActivityId] = useState([]);
+    let direction = '';
     // swipe cards direction
     useEffect(() => {
         axios
@@ -22,35 +22,41 @@ export default function ViewActivities() {
             setFavActivities((favActivities) => {
                 return [...favActivities, activity];
             });
+            direction = 'right';
             // console.log(activities);
+        } else {
+            direction = 'left';
         }
+
         console.log(activity);
     };
 
     console.log(favActivities);
 
     // update favActivities
-    // useEffect(() => {
-    //     // PUT request using fetch with async/await
-    //     const updateFavActivities = async () => {
-    //         const requestActivity = {
-    //             method: 'PUT',
-    //             headers: { 'Content-Type': 'application/json' },
-    //             body: JSON.stringify({
-    //                 title: 'React Hooks PUT Request Example',
-    //             }),
-    //         };
-    //         const response = await fetch(
-    //             // link to be changed
-    //             'https://activity-app-backend.herokuapp.com/activities/',
-    //             requestActivity
-    //         );
-    //         const data = await response.json();
-    //         setActivityId(data.id);
-    //     };
+    useEffect(() => {
+        // PUT request using fetch with async/await
+        const updateFavActivities = async () => {
+            const requestActivity = {
+                method: 'PUT',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({
+                    userId: '2',
+                    activityId: 1,
+                    role: direction === 'right' ? 'interested' : 'uninterested',
+                }),
+            };
+            const response = await fetch(
+                // link to be changed
+                'https://activity-app-backend.herokuapp.com/participants',
+                requestActivity
+            );
+            const data = await response.json();
+            setActivityId(data.id);
+        };
 
-    //     updateFavActivities();
-    // }, []);
+        updateFavActivities();
+    }, [direction]);
 
     return (
         <div>
