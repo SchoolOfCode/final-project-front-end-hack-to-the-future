02842a024154df85768ed6edd43f52a87cc4d946
Ruben Gonzalez <rubengon.uk@gmail.com>
2022-02-25 14:34:29 +0000

    activity item and expand funtionallity done

--- src/components/ActivityListItem/index.js
+++ src/components/ActivityListItem/index.js
@@ -1,11 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ActivityCard from '../ActivityCard/index'
 import Button from '../Button/index'
+import "./ActivityListItem.css";
 
-function ActivityListItem() {
+
+function ActivityListItem({activity}) {
+
+  const [ifExpanded , setIfExpanded] = useState(false);
+  console.log(ifExpanded);
+
+  function toggleIfExpanded() {
+     setIfExpanded(!ifExpanded)
+  }
+
   return (
-    <div>ActivityListItem</div>
-  )
+    <li>
+      <h2>{activity.type}</h2>
+      <h3>{activity.date_time}</h3>
+      <Button button="Expand" onClick={toggleIfExpanded} />
+      <div className={ifExpanded ? "expanded" : "collapsed"}>
+        <ActivityCard activity={activity} />
+        <Button button="Collapse" onClick={toggleIfExpanded} />
+        <Button button="Attend" />
+      </div>
+    </li>
+  );
 }
 
 export default ActivityListItem
--- src/components/InterestActivities/index.js
+++ src/components/InterestActivities/index.js
@@ -1,10 +1,27 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import ActivityListItem from '../ActivityListItem/index'
+import axios from "axios";
 
 function InterestActivities() {
+  
+  const [interestedActivities , setInterestedActivities] = useState([]);
+  console.log(interestedActivities);
+   useEffect(() => {
+     axios
+       .get("https://activity-app-backend.herokuapp.com/activities")
+       .then((res) => setInterestedActivities(res.data.payload));
+   },[]);
+
+
   return (
-    <div className='card'>InterestActivities</div>
+    <ul>
+     {interestedActivities.map((activity, index) => {
+       return (
+         <ActivityListItem activity={activity} key={index}/>
+       )
+     })}
+    </ul>
+    
   )
 }
-
 export default InterestActivities
