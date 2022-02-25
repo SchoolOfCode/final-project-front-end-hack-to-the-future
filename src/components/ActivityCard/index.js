b36985158d30d88e63f57e25fa5d6e61ac525bd0
Ruben Gonzalez <rubengon.uk@gmail.com>
2022-02-25 10:08:33 +0000

    activity card styled

--- src/components/ActivityCard/index.js
+++ src/components/ActivityCard/index.js
@@ -1,16 +1,43 @@
-import React from 'react';
-import TinderCard from 'react-tinder-card';
+import React from "react";
+import activityImage from "../../images/tennis.jpg";
+import "./ActivityCard.css";
 
 function ActivityCard({ activity }) {
-    return (
-        <div className='card'>
-            <img src=''></img>
-            <h3>{activity.description}</h3>
-            <p>Max attendees {activity.max_attendees}</p>
-            <p>{activity.location_name}</p>
-            <p>{activity.date_time}</p>
-        </div>
-    );
+
+
+  const [date , time] = activity.date_time.split("T");
+  const hourstime = time.slice(0,5);
+
+  return (
+    <div className="card">
+      <div className="imageFrame">
+        <img src={activityImage} alt="activity representation" />
+      </div>
+      <h3>{activity.type}</h3>
+      <p>{activity.description}</p>
+      <div className="spanned">
+        <span>
+          <b>Location: </b>
+          {activity.location}
+        </span>
+        <span>
+          {" "}
+          <b>Max attendees: </b>
+          {activity.max_attendees}
+        </span>
+      </div>
+      <div className="spanned">
+        <span>
+          <b>Date: </b>
+          {date}
+        </span>
+        <span>
+          <b>Time: </b>
+          {hourstime} H
+        </span>
+      </div>
+    </div>
+  );
 }
 
 export default ActivityCard;
