3d5f1a06f2aef6ebdfee43dc97f0595a19c44c12
Corneliu Laur <cl179971@gmail.com>
2022-02-25 16:22:05 +0000

    Activity List Item GET and PUT requests made

--- src/components/InterestActivities/index.js
+++ src/components/InterestActivities/index.js
@@ -1,27 +1,39 @@
-import React, { useState, useEffect } from 'react'
-import ActivityListItem from '../ActivityListItem/index'
-import axios from "axios";
+import React, { useState, useEffect } from 'react';
+import ActivityListItem from '../ActivityListItem/index';
+import axios from 'axios';
 
 function InterestActivities() {
-  
-  const [interestedActivities , setInterestedActivities] = useState([]);
-  console.log(interestedActivities);
-   useEffect(() => {
-     axios
-       .get("https://activity-app-backend.herokuapp.com/activities")
-       .then((res) => setInterestedActivities(res.data.payload));
-   },[]);
+    const [interestedActivities, setInterestedActivities] = useState([]);
+    console.log(interestedActivities);
 
 
-  return (
-    <ul>
-     {interestedActivities.map((activity, index) => {
-       return (
-         <ActivityListItem activity={activity} key={index}/>
-       )
-     })}
-    </ul>
-    
-  )
+    useEffect(() => {
+        const getParticipants = async () => {
+            const requestParticipants = {
+                method: 'PATCH',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({
+                    user_id: '4',
+                    participant_role: 'interested',
+                }),
+            };
+
+            const response = await fetch(
+                'https://activity-app-backend.herokuapp.com/participants',
+                requestParticipants
+            );
+            const data = await response.json();
+            setInterestedActivities(data.payload);
+        };
+        getParticipants();
+    }, []);
+
+    return (
+        <ul>
+            {interestedActivities.map((activity, index) => {
+                return <ActivityListItem activity={activity} key={index} />;
+            })}
+        </ul>
+    );
 }
-export default InterestActivities
+export default InterestActivities;
--- src/components/ViewActivities/index.js
+++ src/components/ViewActivities/index.js
@@ -1,74 +1,74 @@
-import React from "react";
-import ActivityCard from "../ActivityCard/index";
-import TinderCard from "react-tinder-card";
-import { useState, useEffect } from "react";
-import "./styles.css";
-import axios from "axios";
-import FilterComponent from "../FilterComponent";
+import React from 'react';
+import ActivityCard from '../ActivityCard/index';
+import TinderCard from 'react-tinder-card';
+import { useState, useEffect } from 'react';
+import './styles.css';
+import axios from 'axios';
+import FilterComponent from '../FilterComponent';
 
 export default function ViewActivities() {
-  const [activities, setActivity] = useState([]);
-  const [currentSwipedCard, setCurrentSwipedCard] = useState({
-    direction: null,
-    activityId: null,
-  });
-
-  useEffect(() => {
-    axios
-      .get("https://activity-app-backend.herokuapp.com/activities")
-      .then((res) => setActivity(res.data.payload));
-  }, []);
-
-  const Swiped = (direction, activity) => {
-    setCurrentSwipedCard({
-      direction: direction,
-      activityId: activity.activity_id,
+    const [activities, setActivity] = useState([]);
+    const [currentSwipedCard, setCurrentSwipedCard] = useState({
+        direction: null,
+        activityId: null,
     });
-  };
 
-  // update favActivities
-  useEffect(() => {
-    // PUT request using fetch with async/await
-    const updateFavActivities = async () => {
-      const requestActivity = {
-        method: "PUT",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({
-          userId: "2",
-          activityId: currentSwipedCard.activityId,
-          role:
-            currentSwipedCard.direction === "right"
-              ? "interested"
-              : "uninterested",
-        }),
-      };
-      const response = await fetch(
-        // link to be changed
-        "https://activity-app-backend.herokuapp.com/participants",
-        requestActivity
-      );
-      const data = await response.json();
-      console.log(data);
+    useEffect(() => {
+        axios
+            .get('https://activity-app-backend.herokuapp.com/activities')
+            .then((res) => setActivity(res.data.payload));
+    }, []);
+
+    const Swiped = (direction, activity) => {
+        setCurrentSwipedCard({
+            direction: direction,
+            activityId: activity.activity_id,
+        });
     };
 
-    if (currentSwipedCard.activityId) {
-      updateFavActivities();
-    }
-  }, [currentSwipedCard]);
+    // update favActivities
+    useEffect(() => {
+        // PUT request using fetch with async/await
+        const updateFavActivities = async () => {
+            const requestActivity = {
+                method: 'PUT',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({
+                    userId: '2',
+                    activityId: currentSwipedCard.activityId,
+                    role:
+                        currentSwipedCard.direction === 'right'
+                            ? 'interested'
+                            : 'uninterested',
+                }),
+            };
+            const response = await fetch(
+                // link to be changed
+                'https://activity-app-backend.herokuapp.com/participants',
+                requestActivity
+            );
+            const data = await response.json();
+            console.log(data);
+        };
 
-  return (
-    <div>
-      <FilterComponent />
-      {activities.map((activity, index) => (
-        <TinderCard
-          className="swipe"
-          key={index}
-          preventSwipe={["up", "down"]}
-          onSwipe={(dir) => Swiped(dir, activity)}
-        >
-          <ActivityCard activity={activity} />
-        </TinderCard>
-      ))}
-    </div>
-  );
+        if (currentSwipedCard.activityId) {
+            updateFavActivities();
+        }
+    }, [currentSwipedCard]);
+
+    return (
+        <div>
+            <FilterComponent />
+            {activities.map((activity, index) => (
+                <TinderCard
+                    className='swipe'
+                    key={index}
+                    preventSwipe={['up', 'down']}
+                    onSwipe={(dir) => Swiped(dir, activity)}
+                >
+                    <ActivityCard activity={activity} />
+                </TinderCard>
+            ))}
+        </div>
+    );
 }
