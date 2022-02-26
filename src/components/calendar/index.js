2b7196ca8c9291776aeb1fb2a20f586b9fdcf3aa
Maria-C1 <mariacrawford789@gmail.com>
2022-02-26 21:22:08 +0000

    activity images added

--- src/components/calendar/index.js
+++ src/components/calendar/index.js
@@ -1,8 +1,15 @@
 import React from "react";
 import ActivityListItem from "../ActivityListItem/index";
+import Kalend, { CalendarView } from "kalend"; // import component
+import "kalend/dist/styles/index.css"; // import styles
 
 function Calendar() {
-  return <div>This is where the My Activities Calendar component will be!</div>;
+  return (
+    <div>
+      This is where the My Activities Calendar component will be!
+      <Kalend initialView={CalendarView.MONTH} />
+    </div>
+  );
 }
 
 export default Calendar;
