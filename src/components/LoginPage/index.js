7e8d19ce07688160abfa0b394954292629729197
casey <cassandra.d.mclellan@gmail.com>
2022-02-28 10:19:36 +0000

    FEAT: added Auth0 login/logout buttons and profile display

---
+++ src/components/LoginPage/index.js
@@ -0,0 +1,15 @@
+import LoginButton from "./LoginButton";
+import LogoutButton from "./LogoutButton";
+import Profile from "./Profile";
+
+function LoginPage() {
+  return (
+    <div>
+      <LoginButton />
+      <LogoutButton />
+      <Profile />
+    </div>
+  );
+}
+
+export default LoginPage;
--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -1,11 +1,12 @@
-import React from 'react'
+import React from "react";
 import { Routes, Route } from "react-router-dom";
 //import Introduction from '../Introduction/index'
-import Home from '../Home/index'
-import ViewActivities from '../ViewActivities/index'
-import CreateActivity from '../CreateActivity/index'
-import InterestActivities from '../InterestActivities/index'
-import ActivityCalendar from '../ActivityCalendar/index'
+import Home from "../Home/index";
+import ViewActivities from "../ViewActivities/index";
+import CreateActivity from "../CreateActivity/index";
+import InterestActivities from "../InterestActivities/index";
+import ActivityCalendar from "../ActivityCalendar/index";
+import LoginPage from "../LoginPage";
 
 function Main() {
   return (
@@ -13,27 +14,16 @@ function Main() {
       <Routes>
         <Route
           path="/"
-          element={<Home/>}
+          // element={<Home/>}
+          element={<LoginPage />}
         />
-        <Route 
-          path="/view-activities"
-          element={<ViewActivities/>}
-        />
-        <Route
-          path="/create-activity"
-          element={<CreateActivity/>}
-        />
-        <Route
-          path="/interest-activities"
-          element={<InterestActivities/>}
-        />
-        <Route
-          path="/activity-calendar"
-          element={<ActivityCalendar/>}
-        />   
+        <Route path="/view-activities" element={<ViewActivities />} />
+        <Route path="/create-activity" element={<CreateActivity />} />
+        <Route path="/interest-activities" element={<InterestActivities />} />
+        <Route path="/activity-calendar" element={<ActivityCalendar />} />
       </Routes>
     </main>
-  )
+  );
 }
 
-export default Main
+export default Main;
