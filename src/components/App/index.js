fced95b7e98019f037ad5f807a822b9c1b5c94a7
Maria-C1 <mariacrawford789@gmail.com>
2022-02-28 12:22:51 +0000

    limited page views for un-authenticated users

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,20 +1,28 @@
-
-import React from 'react'
-import Header from '../Header/index'
-import Main from '../Main/index'
+import React from "react";
+import Header from "../Header/index";
+import Main from "../Main/index";
 import "./App.css";
-import theme from '../../theme'
-import { ThemeProvider } from '@mui/material/styles'
+import theme from "../../theme";
+import { ThemeProvider } from "@mui/material/styles";
+import { useAuth0 } from "@auth0/auth0-react";
+import LoginPage from "../LoginPage";
 
 function App() {
+  const { user, isAuthenticated, isLoading } = useAuth0();
   return (
     <React.StrictMode>
-    <div className="App">
-      <ThemeProvider theme={theme}>
-      <Header  />
-      <Main />
-    </ThemeProvider>
-    </div>
+      <div className="App">
+        <ThemeProvider theme={theme}>
+          {isAuthenticated ? (
+            <>
+              <Header />
+              <Main />
+            </>
+          ) : (
+            <LoginPage />
+          )}
+        </ThemeProvider>
+      </div>
     </React.StrictMode>
   );
 }
--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -6,15 +6,12 @@ import ViewActivities from "../ViewActivities/index";
 import CreateActivity from "../CreateActivity/index";
 import InterestActivities from "../InterestActivities/index";
 import ActivityCalendar from "../ActivityCalendar/index";
-import LoginPage from "../LoginPage";
-import { useAuth0 } from "@auth0/auth0-react";
 
 function Main() {
-  const { isAuthenticated } = useAuth0();
   return (
     <main>
       <Routes>
-        <Route path="/" element={isAuthenticated ? <Home /> : <LoginPage />} />
+        <Route path="/" element={<Home />} />
         <Route path="/view-activities" element={<ViewActivities />} />
         <Route path="/create-activity" element={<CreateActivity />} />
         <Route path="/interest-activities" element={<InterestActivities />} />
@@ -25,3 +22,4 @@ function Main() {
 }
 
 export default Main;
+
