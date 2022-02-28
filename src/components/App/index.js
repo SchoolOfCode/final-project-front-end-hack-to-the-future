ddda781c00aed66898a9c28512f0891253f0cb98
casey <cassandra.d.mclellan@gmail.com>
2022-02-28 15:57:56 +0000

    CHORE: made skeleton useEffect for PUT /users request

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Header from "../Header/index";
 import Main from "../Main/index";
 import "./App.css";
@@ -8,7 +8,14 @@ import { useAuth0 } from "@auth0/auth0-react";
 import LoginPage from "../LoginPage";
 
 function App() {
-  const {isAuthenticated } = useAuth0();
+  const { isAuthenticated, user } = useAuth0();
+
+  useEffect(() => {
+    //send PUT request to /users with the user's auth0 id
+    //backend will create a new user if needed
+    console.log("user object", user);
+    if (user) console.log("user id", user.sub);
+  }, [user]);
   return (
     <React.StrictMode>
       <div className="App">
