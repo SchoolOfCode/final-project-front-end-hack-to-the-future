b7f588ab9666b4b43680c95d7519df224148f219
Maria-C1 <mariacrawford789@gmail.com>
2022-02-28 10:46:19 +0000

    log out added to menu

--- src/components/LoginPage/index.js
+++ src/components/LoginPage/index.js
@@ -1,12 +1,10 @@
 import LoginButton from "./LoginButton";
-import LogoutButton from "./LogoutButton";
 import Profile from "./Profile";
 
 function LoginPage() {
   return (
     <div>
       <LoginButton />
-      <LogoutButton />
       <Profile />
     </div>
   );
