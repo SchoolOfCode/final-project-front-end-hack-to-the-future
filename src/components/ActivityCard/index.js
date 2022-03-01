c1307d7d1d787a54852f9d95ecfc2dc9ac767111
Corneliu Laur <cl179971@gmail.com>
2022-03-01 15:43:28 +0000

    invisible buttons added to Activity Card

--- src/components/ActivityCard/index.js
+++ src/components/ActivityCard/index.js
@@ -1,41 +1,52 @@
-import React from "react";
-import activityImage from "../../images/Activities/tennis.jpg";
-import "./ActivityCard.css";
+import React from 'react';
+import activityImage from '../../images/Activities/tennis.jpg';
+import './ActivityCard.css';
+import Button from '../Button';
 
-function ActivityCard({ activity }) {
-  const [date, time] = activity.date_time.split("T");
-  const hourstime = time.slice(0, 5);
+function ActivityCard({ activity, leftButton, rightButton }) {
+    const [date, time] = activity.date_time.split('T');
+    const hourstime = time.slice(0, 5);
 
-  return (
-    <div className="card">
-      <div className="imageFrame">
-        <img src={activityImage} alt="activity representation" />
-      </div>
-      <h3>{activity.type}</h3>
-      <p>{activity.description}</p>
-      <div className="spanned">
-        <span>
-          <b>Location: </b>
-          {activity.location}
-        </span>
-        <span>
-          {" "}
-          <b>Max attendees: </b>
-          {activity.max_attendees}
-        </span>
-      </div>
-      <div className="spanned">
-        <span>
-          <b>Date: </b>
-          {date}
-        </span>
-        <span>
-          <b>Time: </b>
-          {hourstime} H
-        </span>
-      </div>
-    </div>
-  );
+    return (
+        <div className='card'>
+            <div className='imageFrame'>
+                <img src={activityImage} alt='activity representation' />
+            </div>
+            <h3>{activity.type}</h3>
+            <p>{activity.description}</p>
+            <div className='spanned'>
+                <span>
+                    <b>Location: </b>
+                    {activity.location}
+                </span>
+                <span>
+                    {' '}
+                    <b>Max attendees: </b>
+                    {activity.max_attendees}
+                </span>
+            </div>
+            <div className='spanned'>
+                <span>
+                    <b>Date: </b>
+                    {date}
+                </span>
+                <span>
+                    <b>Time: </b>
+                    {hourstime} H
+                </span>
+            </div>
+
+            {leftButton && (
+                <Button button={leftButton.text} onClick={leftButton.onClick} />
+            )}
+            {rightButton && (
+                <Button
+                    button={rightButton.text}
+                    onClick={rightButton.onClick}
+                />
+            )}
+        </div>
+    );
 }
 
 export default ActivityCard;
