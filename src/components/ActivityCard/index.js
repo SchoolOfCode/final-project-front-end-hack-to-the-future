c7510f1540eb493c21e980aae9845acd7b692098
Maria-C1 <mariacrawford789@gmail.com>
2022-03-01 14:13:56 +0000

    activity type image solution attempt

--- src/components/ActivityCard/index.js
+++ src/components/ActivityCard/index.js
@@ -1,14 +1,24 @@
 import React from "react";
-import activityImage from "../../images/Activities/tennis.jpg";
+// import activityImage from "../../images/Activities/tennis.jpg";
+import walkImg from "../../images/Activities/walk.jpg";
 import "./ActivityCard.css";
 
 function ActivityCard({ activity }) {
   const [date, time] = activity.date_time.split("T");
   const hourstime = time.slice(0, 5);
+  let image;
+  switch (activity.type) {
+    case activity.type === "Walk":
+      image = walkImg;
+      break;
+    default:
+    // do nothing
+  }
   return (
     <div className="card">
       <div className="imageFrame">
-        <img src={activityImage} alt="activity representation" />
+        <img src={image} alt="activity representation" />
+        {/* <img src={activityImage} alt="activity representation" /> */}
       </div>
       <h3>{activity.type}</h3>
       <p>{activity.description}</p>
