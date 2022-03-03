b3daccfdbe5501c9df898621e08ec169660dfd45
casey <cassandra.d.mclellan@gmail.com>
2022-03-03 12:10:21 +0000

    FIX: madee ActivityCard image alt text the activity type

--- src/components/ActivityCard/index.js
+++ src/components/ActivityCard/index.js
@@ -65,7 +65,7 @@ function ActivityCard({ activity, leftButton, rightButton }) {
   return (
     <div className={css.card}>
       <div className={css.imageFrame}>
-        <img src={image} alt="activity representation" />
+        <img src={image} alt={activity.type} />
         {/* <img src={activityImage} alt="activity representation" /> */}
       </div>
       <h3>{activity.type}</h3>
