59d46e60ee6c2739e0e0dcd0a345b2b789f647a0
Kieran Megahey <kieranmegahey2@gmail.com>
2022-03-08 21:50:39 +1100

    changed calendar to display type instead of description

--- src/HelperFunctions/index.js
+++ src/HelperFunctions/index.js
@@ -21,7 +21,7 @@ export function converDateTime(dateTime) {
   return [date, hours];
 }
 
-export function convertActivitiesToEvents(activity ,user_id) {
+export function convertActivitiesToEvents(activity, user_id) {
   function Add2hours() {
     // first to get the hours character
     // convert those character to number
@@ -47,7 +47,7 @@ export function convertActivitiesToEvents(activity ,user_id) {
     return timeString.slice(0, 11) + newTime.toString() + timeString.slice(13);
   }
 
-  let color = activity.organiser_id === user_id? "blue" : "orange";
+  let color = activity.organiser_id === user_id ? "blue" : "orange";
 
   return {
     id: activity.activity_id,
@@ -55,7 +55,7 @@ export function convertActivitiesToEvents(activity ,user_id) {
     startAt: activity.date_time,
     endAt: Add2hours(),
     timezoneStartAt: "Europe/Berlin", // optional
-    summary: activity.description,
+    summary: activity.type,
     color: color,
     calendarID: "work",
     location_name: activity.location_name,
@@ -65,6 +65,7 @@ export function convertActivitiesToEvents(activity ,user_id) {
     user_id: activity.user_id,
     user_name: activity.user_name,
     email: activity.email,
+    description: activity.description,
   };
 }
 
@@ -76,7 +77,7 @@ export function convertData(activity) {
     date_time: activity.startAt,
     location_name: activity.location_name,
     type: activity.type,
-    description: activity.summary,
+    description: activity.description,
     user_name: activity.user_name,
     email: activity.email,
     user_id: activity.user_id,
