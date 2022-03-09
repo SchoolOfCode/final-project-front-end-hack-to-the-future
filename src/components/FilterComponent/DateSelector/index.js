7f5fdebb76fc6ef85299f53604c615d115c8c512
casey <cassandra.d.mclellan@gmail.com>
2022-03-09 16:16:24 +0000

    CHORE: removed unused imports and commented code

--- src/components/FilterComponent/DateSelector/index.js
+++ src/components/FilterComponent/DateSelector/index.js
@@ -1,55 +1,9 @@
 import * as React from "react";
-import TextField from "@mui/material/TextField";
-import AdapterDateFns from "@mui/lab/AdapterDateFns";
-import LocalizationProvider from "@mui/lab/LocalizationProvider";
-import DatePicker from "@mui/lab/DatePicker";
-import { ThemeProvider } from "@mui/material/styles";
-import { buttonsTheme } from "../../../HelperFunctions";
 import css from "./DateSelector.module.css";
 
 export default function DateSelector({ handleDateChange }) {
-  const [value, setValue] = React.useState(null);
-
-  // return (
-  //   <ThemeProvider theme={buttonsTheme.cancel}>
-  //     <LocalizationProvider dateAdapter={AdapterDateFns}>
-  //       <DatePicker
-  //         // style={{ margin: "1rem" }}
-  //         label="Date"
-  //         value={value}
-  //         type="datetime-local"
-  //         minDate={new Date()}
-  //         onChange={(newValue) => {
-  //           setValue(newValue);
-  //           handleDateChange(newValue); // send date in YYYY-MM-DD format
-  //         }}
-  //         renderInput={(params) => (
-  //           <TextField
-  //             sx={{
-  //               width: 300,
-  //               mt: "1rem",
-  //               bgcolor: "white",
-  //             }}
-  //             // sx={{
-  //             //     width: 400,
-  //             // }}
-  //             {...params}
-  //           />
-  //         )}
-  //       />
-  //     </LocalizationProvider>
-  //   </ThemeProvider>
-  // );
   return (
     <div>
-      {/* <input
-        value={value}
-        onChange={(newValue) => {
-          setValue(newValue);
-          handleDateChange(newValue);
-        }}
-      /> */}
-
       <input
         className={css.input}
         type="date"
