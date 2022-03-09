5293776d02bd5b1baec988a48648a074c52325b2
Maria-C1 <mariacrawford789@gmail.com>
2022-03-09 14:36:06 +0000

    Ts and Cs rendering on login on click

--- src/components/IntroductionMain/index.js
+++ src/components/IntroductionMain/index.js
@@ -1,13 +1,14 @@
 import React, { useState } from "react";
 import logo from "../../images/logo.png";
 import LoginButton from "../LoginPage/LoginButton";
+import TsAndCs from "../TsAndCs";
 
 import css from "./IntroductionMain.module.css";
-
 
 function IntroductionMain() {
   const [checked, setChecked] = useState(false);
   const [isDisabled, setIsDisabled] = useState(true);
+  const [tcVisible, setTCVisible] = useState(false);
 
   const canBeSubmitted = () => {
     return checked ? setIsDisabled(true) : setIsDisabled(false);
@@ -17,6 +18,7 @@ function IntroductionMain() {
     setChecked(!checked);
     return canBeSubmitted();
   };
+
   return (
     <div className={css.container}>
       <div className={css.introCard}>
@@ -27,10 +29,13 @@ function IntroductionMain() {
         </p>
         <p>
           I have read the
-          <span style={{ color: "blue" }}> T&C's</span>
+          <span onClick={() => setTCVisible(true)} style={{ color: "blue" }}>
+            {" "}
+            Ts+Cs
+          </span>
           <input type="checkbox" onClick={handleCheck}></input>
         </p>
-
+        {tcVisible && <TsAndCs />}
         <LoginButton disabled={isDisabled} />
       </div>
     </div>
